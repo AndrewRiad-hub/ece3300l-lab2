b1bb429414709e93e33ae7539b8b13b119274dac
Skeesler5915 <skeesler5915@gmail.com>
2026-09-21 17:45:29 -0700

    Update mult4.v

--- skel/mult4.v
+++ skel/mult4.v
@@ -4,7 +4,7 @@ module mult4 (
 	     output [7:0] X
 	     );
 
- wire [3:0] partial0;
+ 	wire [3:0] partial0;
     wire [3:0] partial1;
     wire [3:0] partial2;
     wire [3:0] partial3;
@@ -13,13 +13,9 @@ module mult4 (
     wire [3:0] sum2;
     wire [3:0] sum3;
 
-    wire carry1;
-    wire carry2;
-    wire carry3;
+	wire carry[2:0];
 
-    wire unused_ovf1;
-    wire unused_ovf2;
-    wire unused_ovf3;
+	wire unused_ovf[2:0];
 
     // Generate the four partial products.
     assign partial0 = A & {4{B[0]}};
@@ -36,8 +32,8 @@ module mult4 (
         .X(partial1),
         .Y({1'b0, partial0[3:1]}),
         .S(sum1),
-        .carryout(carry1),
-        .ovf(unused_ovf1)
+		.carryout(carry[0]),
+		.ovf(unused_ovf[0])
     );
 
     assign X[1] = sum1[0];
@@ -47,8 +43,8 @@ module mult4 (
         .X(partial2),
         .Y({carry1, sum1[3:1]}),
         .S(sum2),
-        .carryout(carry2),
-        .ovf(unused_ovf2)
+		.carryout(carry[1]),
+		.ovf(unused_ovf[1])
     );
 
     assign X[2] = sum2[0];
@@ -58,12 +54,12 @@ module mult4 (
         .X(partial3),
         .Y({carry2, sum2[3:1]}),
         .S(sum3),
-        .carryout(carry3),
-        .ovf(unused_ovf3)
+		.carryout(carry[2]),
+		.ovf(unused_ovf[2])
     );
 
     assign X[6:3] = sum3;
-    assign X[7]   = carry3;
+	assign X[7]   = carry[2];
    
 endmodule // mult4
 
